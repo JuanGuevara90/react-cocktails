e341866b9a09bd12e74d531abc302b539718add8
Luis Cisneros <ext_lucisneros@falabella.cl>
2022-04-14 21:38:27 -0500

    Finish CocktailCard component design

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -1,30 +1,35 @@
 import React from 'react';
 import CocktailModel from '../models/Cocktail';
+import Button from './Button';
 
 interface CocktailCardsProps {
   data: CocktailModel;
+  onDetailClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-const CocktailCard = ({ data }: CocktailCardsProps) => {
-  const { strDrink, strAlcoholic, strGlass, strDrinkThumb } = data;
+const CocktailCard = ({ data, onDetailClick }: CocktailCardsProps) => {
+  const {  idDrink, strDrink, strAlcoholic, strGlass, strDrinkThumb } = data;
 
   return (
-    <div className='w-72 overflow-hidden rounded-lg shadow-lg hover:scale-105'>
+    <div className="w-72 overflow-hidden rounded-lg shadow-lg hover:scale-105">
       <img
-        className='object-cover w-full h-72'
+        className="object-cover w-full h-72"
         src={strDrinkThumb}
-        alt='Flower and sky'
+        alt={`drink-${idDrink}`}
       />
-      <div className='px-6 py-4'>
-        <h4 className='mb-3 text-3xl font-semibold text-gray-800'>
+      <div className="p-3">
+        <h4 className="mb-3 text-3xl font-semibold text-gray-800">
           {strDrink}
         </h4>
-        <p className='leading-normal  text-gray-700'>
+        <p className="text-gray-700">
           {strGlass}
         </p>
-        <p className='leading-normal text-gray-700'>
+        <p className="text-sm text-gray-500">
           {strAlcoholic}
         </p>
+        <div className="flex justify-end">
+          <Button label="See details" onClick={onDetailClick} />
+        </div>
       </div>
     </div>
   );
