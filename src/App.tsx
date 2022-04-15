b44a73acab9ce4e8d5ff003c7111133cdd4c33e5
Juan Pablo Guevara <guevara_juan1990@hotmail.com>
2022-04-14 20:45:29 -0500

    Create a home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import Home from "./pages/Home";
+
 const App = () => {
   return (
     <div className="flex justify-center">
-      <h1 className="font-bold text-2xl text-blue-900">
-        React and Tailwind with Vitejs!
-      </h1>
+      <Home />
     </div>
   );
 };
