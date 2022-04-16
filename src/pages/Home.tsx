3a3cf4ffd2d6f79212c494df89f6195c235a2ac6
Luis Cisneros <ext_lucisneros@falabella.cl>
2022-04-15 21:24:14 -0500

    Perform cocktail search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,33 @@
-import { useEffect, useState } from 'react'
-import { fetchCocktailsByQuery } from '../api/search'
-import CocktailList from '../components/CocktailList'
-import PageTitle from '../components/PageTitle'
-import SearchForm from '../components/SearchForm'
-import CocktailModel from '../models/Cocktail'
+import { useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
 
-type Props = {}
+import { fetchCocktailsByQuery } from "../api/search";
+import CocktailList from "../components/CocktailList";
+import PageTitle from "../components/PageTitle";
+import SearchForm from "../components/SearchForm";
+import CocktailModel from "../models/Cocktail";
 
-const CocktailDemo: CocktailModel = {
-  idDrink: "15082",
-  strDrink: "Royal Flush",
-  strAlcoholic: "Alcoholic",
-  strGlass: "Old-fashioned glass",
-  strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/7rnm8u1504888527.jpg"
-}
-
-const Home = (props: Props) => {
-  const [searchQuery, setSearchQuery] = useState<string>("Margarita");
+const Home = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("Margarita");
   const [cocktails, setCocktails] = useState<CocktailModel[]>([]);
+  const [debouncedSearchTerm] = useDebounce(searchTerm, 500);
 
   useEffect(() => {
-    fetchCocktailsByQuery(searchQuery).then(({ data: { drinks } }) => setCocktails(drinks));
-  }, [searchQuery]);
+    fetchCocktailsByQuery(debouncedSearchTerm).then(({ data: { drinks } }) =>
+      setCocktails(drinks)
+    );
+  }, [debouncedSearchTerm]);
 
   return (
     <>
       <PageTitle text="Cocktail Land" />
-      <SearchForm query={searchQuery} onSearchChange={(value) => setSearchQuery(value)}/>
-      <CocktailList  listCocktails={cocktails}/>
+      <SearchForm
+        query={searchTerm}
+        onSearchChange={(value) => setSearchTerm(value)}
+      />
+      <CocktailList listCocktails={cocktails} />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
